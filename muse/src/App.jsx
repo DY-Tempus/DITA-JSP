b672056a210ab66e312f19c585a051141113ec2e
BENTRO <37620584+hyn062347@users.noreply.github.com>
2024-09-23 15:37:47 +0900

    사이드바 만듦

--- muse/src/App.jsx
+++ muse/src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
 import MusicPlayer from './components/MusicPlayer'; // MusicPlayer 컴포넌트도 함께 가져오기
+import Sidebar from './components/Sidebar';
 
 const App = () => {
+
+
     return (
         <div>
             <Header />
             <Home />
+            <Sidebar/>
             <MusicPlayer /> {/* MusicPlayer를 하단에 유지되도록 추가 */}
         </div>
     );
---
+++ muse/src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import './css/Sidebar.css';
+
+function Sidebar({ isOpen }) {
+
+    
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // 사이드바 상태 관리
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+        //버튼 클릭시 사이드바가 나옴
+
+    };
+
+    return (
+            <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}> {/* isOpen 값에 따라 클래스 추가 */}
+                <div className="user-info">
+                    <img src="/path-to-user-icon" alt="User Icon" className="user-icon" />
+                    <h2>User</h2>
+                    <h3>My Page</h3>
+                </div>
+                <ul className="menu">
+                    <li>Profile</li>
+                    <li>Recently Played</li>
+                    <li>Favorite</li>
+                    <li>My Playlist</li>
+                    <li>My Music</li>
+                    <li>Upload Music</li>
+                </ul>
+                <button className="settings-btn">
+                    <img src="/path-to-settings-icon" alt="Settings" />
+                </button>
+                <button className='toggle-btn' onClick={toggleSidebar}></button>
+            </div>
+    );
+}
+
+export default Sidebar;
