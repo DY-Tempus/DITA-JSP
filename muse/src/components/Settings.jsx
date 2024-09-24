5442a90c94456f656ad148cf476c3cc0cafd59b3
bbaginuk <pbh110708@gmail.com>
2024-09-24 12:09:08 +0900

    Sidebar랑 Settings 연동

--- muse/src/components/Settings.jsx
+++ muse/src/components/Settings.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './css/Settings.css'; // CSS 파일 import
+import ReactDOM from 'react-dom';
+import './css/Settings.css';
 
-const Settings = () => {
-    return (
-        <div className="settings-container">
+const Settings = ({ isOpen, setIsOpen }) => {
+
+    // Settings 패널을 숨기는 함수
+    const hideSettings = () => {
+        setIsOpen(false); // Settings를 숨김
+    };
+
+    return ReactDOM.createPortal(
+        <div className={`settings-panel ${isOpen ? 'open' : ''}`}>
             <div className="settings-header">
-            <img src="/img/back.png" alt="Back" className="back" onClick={() => console.log('Back clicked')} />
+            <img src="/img/back.png" alt="Back" className="back" onClick={hideSettings} />
                 <h2>Settings</h2>
             </div>
             <div className="settings-item">
@@ -19,12 +26,6 @@ const Settings = () => {
             </div>
             <div className="settings-item">
                 <span>시청 기록 관리</span>
-                <div className="toggle-switch">
-                    <label className="switch">
-                        <input type="checkbox" />
-                        <span></span>
-                    </label>
-                </div>
             </div>
             <div className="settings-item">
                 <span>시청 기록 일시중지</span>
@@ -47,7 +48,8 @@ const Settings = () => {
             <div className="settings-item">
                 <span>Project 정보</span>
             </div>
-        </div>
+        </div>,
+        document.body
     );
 };
 
