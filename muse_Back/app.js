5408748ad540e6250437461d741ba5335fac843e
jeongerased <112837517+jeongerased@users.noreply.github.com>
2024-09-26 16:49:09 +0900

    지운
    app.js 클라이언트 연결

--- muse_Back/app.js
+++ muse_Back/app.js
@@ -1,6 +1,6 @@
 // app.js에서 database.js를 import (최상단에 추가)
 const db = require('./database');
-
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000; // 사용할 포트 번호
@@ -9,10 +9,7 @@ const port = 3000; // 사용할 포트 번호
 app.use(express.json()); // JSON 데이터 처리
 app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 처리
 
-// 기본 테스트 라우터
-app.get('/', (req, res) => {
-  res.send('Express 서버가 실행 중입니다.');
-});
+
 
 // API 라우터 연결
 const userRouter = require('./routers/userRouter'); // 예시로 사용자 라우터 추가
@@ -21,4 +18,16 @@ app.use('/api/users', userRouter); // 사용자 라우터 설정
 // 서버 시작
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
+
+app.use(express.static(path.join(__dirname, 'muse/build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/muse/build/index.html'));
+});
+
+
+
+
+app.get('*', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, 'muse/build/index.html'));
 });
