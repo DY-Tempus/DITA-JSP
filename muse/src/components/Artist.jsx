a37fc8a75cd487ca10394de795f675dab546a954
BENTRO <37620584+hyn062347@users.noreply.github.com>
2024-09-26 14:24:03 +0900

    Update Artist.jsx

--- muse/src/components/Artist.jsx
+++ muse/src/components/Artist.jsx
@@ -1,91 +1,69 @@
 import React from 'react';
 import './css/Artist.css';
 
+// 트랙 데이터 선언
+const tracks = [
+  {
+    id: 1,
+    title: "WIP That'll Never Come Out",
+    duration: "2:48",
+    image: "/img/getsixart1.png"
+  },
+  // 추가 트랙 데이터들
+  {
+    id: 2,
+    title: "The Psychedelic Experience",
+    duration: "4:35",
+    image: "/img/getsixart1.png"
+  },
+  {
+    id: 3,
+    title: "Astral Projection",
+    duration: "5:53",
+    image: "/img/getsixart1.png"
+  },
+  // 다른 트랙도 여기에 추가 가능
+];
+
 const Artist = () => {
-    return (
-        <div className="artist-page">
-            {/* 아티스트 정보 */}
-            <div className="artist-info">
-                <img src="/img/getsix.png" alt="Artist" className="artist-image" />
-                <div className="artist-details">
-                    <div className="social-icons">
-                        <h1>Getsix</h1>
-                        <img src="/img/YouTubeLogo.png" alt="YouTube" />
-                        <img src="/img/XLogo.png" alt="Twitter" />
-                    </div>
-                    <p>7,603 Subs</p>
-                    <button className="subscribe-btn">Subscribe</button>
-                </div>
-
-            </div>
-
-            {/* 트랙 리스트 */}
-            <div className="tracklist">
-                <h2>
-                    <select id="sort" name="fruit">
-                        <option value="Recent">Recent</option>
-                        <option value="View">View</option>
-                        <option value="Comment">Comment</option>
-                        <option value="Date">Date</option>
-                    </select>
-                </h2>
-                <ul>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    <li>
-                        <img src="/img/getsixart1.png" alt="Track" className="track-image" />
-                        <span className="track-title">WIP That'll Never Come Out</span>
-                        <span className="track-duration">2:48</span>
-                    </li>
-                    {/* 나머지 트랙들도 동일한 방식으로 추가 */}
-                </ul>
-            </div>
+  return (
+    <div className="artist-page">
+      {/* 아티스트 정보 */}
+      <div className="artist-info">
+        <img src="/img/getsix.png" alt="Artist" className="artist-image" />
+        <div className="artist-details">
+          <div className="social-icons">
+            <h1>Getsix</h1>
+            <img src="/img/YouTubeLogo.png" alt="YouTube" />
+            <img src="/img/XLogo.png" alt="Twitter" />
+          </div>
+          <p>7,603 Subs</p>
+          <button className="subscribe-btn">Subscribe</button>
         </div>
+      </div>
 
-    );
+      {/* 트랙 리스트 */}
+      <div className="tracklist">
+        <h2>
+          <select id="sort" name="artist-sort">
+            <option value="Recent">Recent</option>
+            <option value="View">View</option>
+            <option value="Comment">Comment</option>
+            <option value="Date">Date</option>
+          </select>
+        </h2>
+        <ul>
+          {tracks.map((track) => (
+            <li key={track.id}>
+              <img src={track.image} alt={track.title} className="track-image" />
+              <span className="track-title">{track.title}</span>
+              <span className="track-duration">{track.duration}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Artist;
