b97736a4fdaff37073131cb587b3c913668a104e
jeongerased <112837517+jeongerased@users.noreply.github.com>
2024-10-11 13:30:41 +0900

    내가해냄

--- muse_Back/controllers/detailController.js
+++ muse_Back/controllers/detailController.js
@@ -2,7 +2,26 @@ const pool = require('../db');
 
 const getMusic =(req, res) => {
 
-    const sql=`SELECT mid, mname, mlyrics, name AS uname, COALESCE(aname, 'No Album') AS aname FROM music JOIN user ON music.ID=user.ID LEFT JOIN album ON music.aID=album.aID WHERE mid=${req.query.mid};`;
+    const sql=`SELECT mid, mname, mimg , mlyrics, name AS uname, COALESCE(aname, 'No Album') AS aname FROM music JOIN user ON music.ID=user.ID LEFT JOIN album ON music.aID=album.aID WHERE mid=${req.body.mid};`;
+    pool.getConnection((error,connection)=>{
+        if (error) {
+            return res.status(500).json({ error: '음악 조회 실패' });
+        }
+
+        connection.query(sql,(error,result)=>{
+            if(!error){
+                res.send(JSON.stringify(result));
+                connection.release();
+            }else{
+                throw error
+            }
+        })
+
+    })
+};
+const getComment =(req, res) => {
+
+    const sql=`select * from comment where mid=${req.body.mid}`;
     pool.getConnection((error,connection)=>{
         if (error) {
             return res.status(500).json({ error: '음악 조회 실패' });
@@ -22,4 +41,5 @@ const getMusic =(req, res) => {
 
 module.exports = { 
     getMusic,
+    getComment
  };
